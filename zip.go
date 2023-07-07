81a5f979b93ecb2566fc299ab95ff1c9335eadb7
Ned Bingham <edward.bingham@broccolimicro.io>
2023-07-07 09:36:10 -0400

    making the zipper more efficient with a quick sort of length

--- zip.go
+++ zip.go
@@ -1,16 +1,36 @@
 package iter
 
+import "sort"
+
 type Zipper[T interface{}] struct {
 	Slices [][]T
 	Index int
 	Count int
 }
 
+func (i *Zipper[T]) Len() int {
+	return len(i.Slices)
+}
+
+func (i *Zipper[T]) Swap(j, k int) {
+	i.Slices[j], i.Slices[k] = i.Slices[k], i.Slices[j]
+}
+
+func (i *Zipper[T]) Less(j, k int) bool {
+	return len(i.Slices[j]) > len(i.Slices[k])
+}
+
 func Zip[T interface{}](Slices ...[]T) *Zipper[T] {
-	return &Zipper[T]{
+	result := &Zipper[T]{
 		Slices: Slices, 
 		Count: -1,
 	}
+	for _, s := range Slices {
+		if result.Count < len(s) {
+			result.Count = len(s)
+		}
+	}
+	return result
 }
 
 func ZipIterables[T interface{}](Slice ...Iterable[T]) *Zipper[T] {
@@ -19,19 +39,17 @@ func ZipIterables[T interface{}](Slice ...Iterable[T]) *Zipper[T] {
 		Count: -1,
 	}
 	for _, s := range Slice {
-		result.Slices = append(result.Slices, s.Iterate().Slice)
+		i := s.Iterate()
+		result.Slices = append(result.Slices, i.Slice)
+		if result.Count < i.Size() {
+			result.Count = i.Size()
+		}
 	}
+	sort.Sort(result)
 	return result
 }
 
 func (i *Zipper[T]) Size() int {
-	if i.Count < 0 {
-		for j := 0; j < len(i.Slices); j++ {
-			if i.Count < len(i.Slices[j]) {
-				i.Count = len(i.Slices[j])
-			}
-		}
-	}
 	return i.Count
 }
 
@@ -46,9 +64,10 @@ func (i *Zipper[T]) Next() {
 func (i *Zipper[T]) Get() []T {
 	result := make([]T, 0, len(i.Slices))
 	for _, slice := range i.Slices {
-		if i.Index < len(slice) {
-			result = append(result, slice[i.Index])
+		if i.Index >= len(slice) {
+			return result
 		}
+		result = append(result, slice[i.Index])
 	}
 	return result
 }
@@ -56,9 +75,10 @@ func (i *Zipper[T]) Get() []T {
 func (i *Zipper[T]) Ptr() []*T {
 	result := make([]*T, 0, len(i.Slices))
 	for _, slice := range i.Slices {
-		if i.Index < len(slice) {
-			result = append(result, &slice[i.Index])
+		if i.Index >= len(slice) {
+			return result
 		}
+		result = append(result, &slice[i.Index])
 	}
 	return result
 }
